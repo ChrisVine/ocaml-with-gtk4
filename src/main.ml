df331f01de4b69c9a7d153d6eea6d322e63b1902
Chris Vine <vine.chris@gmail.com>
2024-12-01 14:30:09 +0000

    Add comments about custom blocks and about atomics

--- src/main.ml
+++ src/main.ml
@@ -53,6 +53,13 @@ let _ =
        we can instead apply 'ref' in the worker thread immediately
        before it applies Dispatcher.post to 'disp').
 
+       For a different approach, it would be possible to hold the
+       underlying GTK widget in a custom block instead of an abstract
+       block, and to unref it in the custom block's finalizer.  Whilst
+       that would be less prone to programmer error, there are
+       downsides to keeping an unreachable GTK widget alive until a
+       garbage collection occurs.
+
        This is a demo.  Applying ref/unref here is in this case
        unnecessary because the Scrolled_text widget stays valid
        throughout program execution anyway. *)
@@ -65,6 +72,12 @@ let _ =
 
   (* see comments on on_activated1 *)
   let on_activated2 =
+    (* beware, if code accessing 'counter' were running in a new
+       domain, 'counter' should be atomic, but as only one Thread.t
+       thread can hold the domain lock at a time, we should be OK in
+       the code below with a plain int reference here (and of course
+       losing the odd increment doesn't matter in this demo); but if
+       in doubt use a mutex or atomics *)
     let counter = ref 0 in
     let disp =
       Dispatcher.make
